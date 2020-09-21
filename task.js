c16f6cdaa6f1724c0728b2c238260f2b2f8d1a75
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2020-09-21 16:59:29 +0300

    hw07

--- task.js
+++ task.js
@@ -1,18 +1,22 @@
-const box = document.querySelector(".box");
+// const box = document.querySelector(".box");
 
-box.style.height = "500px";
-// box.style.background = "black";
-box.style.paddingTop = "250px";
+// box.style.height = "500px";
+// // box.style.background = "black";
+// box.style.paddingTop = "250px";
 
-const button = document.createElement("button");
-button.textContent = "click";
-button.style.width = "300px";
-button.style.display = "block";
-button.style.margin = "0 auto";
+// const button = document.createElement("button");
+// button.textContent = "click";
+// button.style.width = "250px";
+// button.style.display = "block";
+// button.style.margin = "0 auto";
+// button.style.paddingTop = "100px";
+// button.style.paddingBottom = "100px";
+// button.style.borderRadius = "50%";
+// button.style.fontSize = "50px";
 
-console.log(button);
-box.append(button);
-console.log(box);
-button.addEventListener("click", () => {
-  box.classList.toggle("red");
-});
+// console.log(button);
+// box.append(button);
+// console.log(box);
+// button.addEventListener("click", () => {
+//   box.classList.toggle("red");
+// });
